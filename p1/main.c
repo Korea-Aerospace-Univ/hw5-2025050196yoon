55e00614948ab06ece92fc10ef470cc0babe66b9
2025050196yoon <choyoon@kau.kr>
2026-05-04 15:24:14 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -8,7 +8,7 @@ int main(void){
     for(int a=1 ; a*900 ; a++){
         for(int b=1 ; b*70 ; b+=2){
             for(int c=1 ; c*200 ; c++){
-                if(a*900 + b*750 + c*200 = n ){
+                if(a*900 + b*750 + c*200 == n ){
                     if(c < a || c < b){
                         printf("%d %d %d\n", a, b, c);
                         method = 1;
