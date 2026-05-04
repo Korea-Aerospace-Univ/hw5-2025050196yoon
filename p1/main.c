10a65e7ba93c04b042ef982e112279656cd89cca
2025050196yoon <choyoon@kau.kr>
2026-05-04 15:28:55 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -5,14 +5,13 @@ int main(void){
     int method = 0;
     scanf("%d", &n);
     
-    for(int a=1 ; a*900 ; a++){
-        for(int b=1 ; b*70 ; b+=2){
-            for(int c=1 ; c*200 ; c++){
+    for(int a=1 ; a*900 < n ; a++){
+        for(int b=2 ; b*750 < n ; b+=2){
+            for(int c=1 ; c*200 < n ; c++){
                 if(a*900 + b*750 + c*200 == n ){
                     if(c < a || c < b){
                         printf("%d %d %d\n", a, b, c);
                         method = 1;
-                
                     }
                 }
             }
